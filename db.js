89281703eb579c4feed5e0e23280bc31bc4eef5b
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-23 11:45:47 +0000

    update

--- db.js
+++
@@ -1,4 +0,0 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const connectToDynamoDB = new DynamoDBClient({});
-
-module.exports = connectToDynamoDB;
---
+++ src/app.ts
@@ -0,0 +1,1 @@
+import { ddbClient } from './db'
